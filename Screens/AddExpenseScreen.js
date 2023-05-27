3a4a90e8feb3aa7028f9a74e8ee9eeafde71c64d
ganuke <gauke@gmail.com>
2023-05-27 06:42:32 +0530

    App Structure and Main Components are done

---
+++ Screens/AddExpenseScreen.js
@@ -0,0 +1,11 @@
+import { View, StyleSheet } from "react-native";
+
+export default function AddExpenseScreen() {
+  return (
+    <View>
+      <Text>Add!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
