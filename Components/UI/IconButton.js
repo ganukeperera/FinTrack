c4da805be9020ef6ce7f5b0e5d765315ce901d13
ganuke <gauke@gmail.com>
2023-05-28 06:17:47 +0530

    navigaton setup done with dummy data

--- Components/UI/IconButton.js
+++ Components/UI/IconButton.js
@@ -1,13 +1,13 @@
 import { Pressable, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-export default function IconButton({ name, color, onPress }) {
+export default function IconButton({ name, color, onPress, size }) {
   return (
     <Pressable
       style={({ pressed }) => pressed && styles.press}
       onPress={onPress}
     >
-      <Ionicons name={name} color={color} size={24}></Ionicons>
+      <Ionicons name={name} color={color} size={size}></Ionicons>
     </Pressable>
   );
 }
